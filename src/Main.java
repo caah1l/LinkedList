38d56b14c9168ec736bd3f5d8357c063de0ce4c8
Sahil Agrawal <agrawalsahil60@gmail.com>
2024-08-27 22:31:22 +0530

    declaration and addition of elements

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        var placesToVisit = new LinkedList<String>();
+
+        //Adding - somewhere similar to ArrayLists
+        placesToVisit.add("Ladakh");
+        placesToVisit.add(0,"Manali");
+
+    }
+}
